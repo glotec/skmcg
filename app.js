f2e43990873d5b39df07201c24563d4755ab8a88
SHUKURU BAHATI Gloire <gloireshukurubahati@gmail.co>
2019-06-08 14:41:59 +0200

    Changed Port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, 
     console.log(`Server started on port ${PORT}`));
